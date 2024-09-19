a8900678f95fd667bd0bbd40000b26b1ca8746a2
birdinterface <alexgisbrecht20@gmail.com>
2024-09-19 17:19:28 +0200

    Update and rename next.config.cjs to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    CLERK_FRONTEND_API: process.env.CLERK_FRONTEND_API,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
   },
+  experimental: {
+    esmExternals: true
   async headers() {
     return [
       {
