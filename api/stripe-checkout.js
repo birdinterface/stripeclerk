06bd7bbcdb1b19038ad3c16ba8ae27758630ce69
birdinterface <alexgisbrecht20@gmail.com>
2024-09-19 17:20:26 +0200

    Update stripe-checkout.js

--- api/stripe-checkout.js
+++ api/stripe-checkout.js
@@ -1,7 +1,7 @@
 // api/stripe-checkout.mjs
 
-import Stripe from 'stripe';
-import { auth } from '@clerk/nextjs';
+const Stripe = require('stripe');
+const { auth } = require('@clerk/nextjs');
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
