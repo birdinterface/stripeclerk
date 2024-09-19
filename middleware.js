2e1c107c9071e10dfb0c925a35b17781a4f09aac
birdinterface <alexgisbrecht20@gmail.com>
2024-09-19 23:55:58 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,23 @@
 // middleware.js
 
+import { NextResponse } from 'next/server';
+
 export default function middleware(req) {
-  return NextResponse.next();
+  const response = NextResponse.next();
+
+  // Set CORS headers
+  response.headers.set('Access-Control-Allow-Origin', 'https://www.advancers.org');
+  response.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    // Return response for OPTIONS requests with CORS headers
+    return new NextResponse(null, { status: 200, headers: response.headers });
+  }
+
+  return response;
 }
 
 export const config = {
-  matcher: '/((?!_next/static|favicon.ico).*)',
+  matcher: '/api/:path*',
 };
